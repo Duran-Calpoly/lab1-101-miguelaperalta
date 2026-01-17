fb28ac2b85d5fcf2df8a09a016969cc72e794b4f
miguelaperalta <miguelangelperalta1407@gmail.com>
2026-01-16 17:50:10 -0800

    Update lab1_4.py

--- lab1_4.py
+++ lab1_4.py
@@ -1,8 +1,8 @@
 def calculate_average(num1,num2,num3):
-    return (num1+num2+num3)/2
+    return (num1+num2+num3) / 3
 
 def add_tax(bill_total):
-    return bill_total*0.1
+    return bill_total + bill_total * 0.1
 
 def greet_user(name):
     return "Hello " + name
