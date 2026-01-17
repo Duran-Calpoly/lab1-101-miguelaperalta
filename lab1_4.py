9803d0c066961f84fc2a2422d3750276d1e52e13
miguelaperalta <miguelangelperalta1407@gmail.com>
2026-01-17 00:58:06 +0000

    Completed Lab 1

--- lab1_4.py
+++ lab1_4.py
@@ -1,1 +1,8 @@
-pass
+def calculate_average(num1,num2,num3):
+    return (num1+num2+num3)/2
+
+def add_tax(bill_total):
+    return bill_total*0.1
+
+def greet_user(name):
+    return "Hello " + name
